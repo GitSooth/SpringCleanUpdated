692d222866e1223c72f370198b22d6898f2eb031
AnaXisca <37443373+AnaXisca@users.noreply.github.com>
2021-01-08 19:02:26 +0000

    Add files via upload

---
+++ Assets/Scripts/DecisionTree/DTCondiccao.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DTCondiccao : DTNode
+{
+    {
+        private DTNode trueChild, falseChild;
+	private Func<bool> condition;
+
+	public DTCondiccao(Func<bool> condition, DTNode tChild, DTNode fChild)
+	{
+		this.condition = condition;
+		trueChild = tChild;
+		falseChild = fChild;
+	}
+
+	public override void Run()
+	{
+		if (condition()) trueChild.Run();
+		else falseChild.Run();
+	}
+}
+
+
