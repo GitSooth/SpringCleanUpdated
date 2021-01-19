c44007380056e5bd0a043a438c2ab9875c4a75e1
AnaXisca <37443373+AnaXisca@users.noreply.github.com>
2021-01-19 18:55:01 +0000

    Add files via upload
    Adding the code of pogram of each enemy for the decision tree and update the transmiction scenes

--- Assets/Scripts/SceneTransictionsScripts/SceneTransitions.cs
+++ Assets/Scripts/SceneTransictionsScripts/SceneTransitions.cs
@@ -13,15 +13,31 @@ public class SceneTransitions : MonoBehaviour
 		if (Input.GetMouseButtonDown(0))
 		{
 			StartCoroutine(LoadScene().ToString());
-		}
+            StartCoroutine(LoadScene2().ToString());
+            StartCoroutine(LoadScene3().ToString());
+        }
 	}
 
 	IEnumerable LoadScene()
 	{
-		animator.SetTrigger("2nd level Trash");
+		animator.SetTrigger("LevelsTransEnd");
 		yield return new WaitForSeconds(2.0f);
 		SceneManager.LoadScene(SceneTransmission);
 	}
 
 
+    IEnumerable LoadScene2()
+    {
+        animator.SetTrigger("LevelsTransEnd");
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene(SceneTransmission);
+    }
+
+
+    IEnumerable LoadScene3()
+    {
+        animator.SetTrigger("LevelsTransEnd");
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene(SceneTransmission);
+    }
 }
